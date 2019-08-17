da8c043ae3e91aeb77195c2da15d8ad13568ff9c
Anthony Esper <anthony.m.esper@gmail.com>
2019-08-17 17:49:33 -0400

    Update App_config/Startup
    Fixed Startup to update before start of services and test fix for template missing

--- startup.sh
+++ startup.sh
@@ -4,6 +4,11 @@ export CARTO_HOSTNAME=${CARTO_HOSTNAME:=$HOSTNAME}
 
 perl -pi -e 's/cartodb\.localhost/$ENV{"CARTO_HOSTNAME"}/g' /etc/nginx/nginx.conf /cartodb/config/app_config.yml /Windshaft-cartodb/config/environments/development.js
 
+#FORCING HTTPS IN DEV 
+echo "Changing the self.use_https? method in /cartodb/config/initializers/carto_db.rb to return true, so https works in dev."
+sed -i "/def self.use_https\?/,/end/c\  def self.use_https?\n    true\n  end" /cartodb/config/initializers/carto_db.rb
+sed -i 's|http://|https://|g' /Windshaft-cartodb/config/environments/development.js
+
 PGDATA=/var/lib/postgresql
 if [ "$(stat -c %U $PGDATA)" != "postgres" ]; then
 (>&2 echo "${PGDATA} not owned by postgres, updating permissions")
@@ -30,12 +35,6 @@ script/sync_tables_trigger.sh &
 # Recreate api keys in db and redis, so sql api is authenticated
 echo 'delete from api_keys' | psql -U postgres -t carto_db_development
 bundle exec rake carto:api_key:create_default
-
-#FORCING HTTPS IN DEV 
-echo "Changing the self.use_https? method in /cartodb/config/initializers/carto_db.rb to return true, so https works in dev."
-sed -i "/def self.use_https\?/,/end/c\  def self.use_https?\n    true\n  end" /cartodb/config/initializers/carto_db.rb
-sed -i 's|http://|https://|g' /Windshaft-cartodb/config/environments/development.js
-
 
 # bundle exec rake carto:api_key:create_default
 bundle exec thin start --threaded -p 3000 --threadpool-size 5
